bd5961fcd127a4b5cb7f182642b5217619a92f51
Connor Mulholland <mulhollandconnor@icloud.com>
2020-10-01 23:56:24 +0000

    Automatically backed up by Learn

---
+++ lib/jukebox.rb
@@ -0,0 +1,42 @@
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
+def help 
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def list(songs)
+  songs.each.with_index(1) do |song, index|
+    puts "#{index}. #{song}"
+  end 
+end 
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  user_song = gets.strip
+  songs.each do |song|
+    if song == user_song
+      "Playing #{song}"
+    else 
+      "Invalid input, please try again"
+    end 
+  end 
+end 
+
+def exit_jukebox
+  puts "Goodbye"
+end 
+  
